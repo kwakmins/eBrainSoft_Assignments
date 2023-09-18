1fd20877ae7f1336cf3a91b35341da8734d966ad
kawksung <alstjd5479@naver.com>
2023-09-19 08:50:28 +0900

    feat: 카테고리 관련 Entity 및 DAO 생성

---
+++ eb-study-templates-2week/src/main/java/com/ebsoft/ebstudytemplates2week/domain/category/entity/Category.java
@@ -0,0 +1,11 @@
+package com.ebsoft.ebstudytemplates2week.domain.category.entity;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+  private Long categoryId;
+  private String categoryName;
+
+}
