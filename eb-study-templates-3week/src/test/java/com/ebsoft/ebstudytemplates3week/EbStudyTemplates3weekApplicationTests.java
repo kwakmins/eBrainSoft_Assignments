287974b410d25eaeabf888b5ed2a6fcd92a49e2e
kawksung <alstjd5479@naver.com>
2023-09-25 15:16:46 +0900

    chore: 프로젝트 생성

---
+++ eb-study-templates-3week/src/test/java/com/ebsoft/ebstudytemplates3week/EbStudyTemplates3weekApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ebsoft.ebstudytemplates3week;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbStudyTemplates3weekApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
