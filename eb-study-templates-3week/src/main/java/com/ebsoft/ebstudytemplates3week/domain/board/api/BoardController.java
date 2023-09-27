199ced67672662528476185ef8b1d94babbb1de2
kawksung <alstjd5479@naver.com>
2023-09-27 14:31:36 +0900

    feat: 게시판 작성 검증 구현

--- eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/api/BoardController.java
+++ eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/api/BoardController.java
@@ -4,7 +4,7 @@ import com.ebsoft.ebstudytemplates3week.domain.board.application.BoardService;
 import com.ebsoft.ebstudytemplates3week.domain.board.dto.request.BoardWriteDto;
 import com.ebsoft.ebstudytemplates3week.domain.category.application.CategoryService;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -37,9 +37,16 @@ public class BoardController {
   작성 폼으로부터, 게시판을 작성한다.
    */
   @PostMapping("/write")
-  public String writeBoard(@ModelAttribute BoardWriteDto reqDto) {
+  public String writeBoard(@Valid @ModelAttribute BoardWriteDto reqDto) {
     reqDto.setCreatedTime(LocalDateTime.now());
     reqDto.setUpdatedTime(LocalDateTime.now());
+
+    // 비밀번호 확인이 틀린 경우.
+    if (!reqDto.getPassword().equals(reqDto.getPasswordConfirm())) {
+      throw new IllegalArgumentException();
+      // 프론트단에서 이미 제약조건을 걸었는데,
+      // 다른 방식으로 억지로 값을 넣은 대상에게, 친절하게 bindResult로 담아서 줄 필요가 있을까?
+    }
     // log.info(reqDto.toString());
     boardService.addBoard(reqDto);
     return "form/boardWriteForm";
--- eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/dto/request/BoardWriteDto.java
+++ eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/dto/request/BoardWriteDto.java
@@ -1,12 +1,15 @@
 package com.ebsoft.ebstudytemplates3week.domain.board.dto.request;
 
-import com.ebsoft.ebstudytemplates3week.domain.category.dto.CategoryDto;
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
@@ -15,16 +18,27 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class BoardWriteDto {
 
   // 카테고리
+  @NotNull
+  @Positive
   private Long categoryId;
   // 글쓴이
+  @NotBlank
+  @Length(min = 3, max = 5)
   private String user;
   // 비밀번호
+  @NotBlank
+  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{4,16}$")
   private String password;
   // 비밀번호 확인
+  @NotBlank
   private String passwordConfirm;
   // 제목
+  @NotBlank
+  @Length(min = 4, max = 99)
   private String title;
   // 내용
+  @NotBlank
+  @Length(min = 4, max = 2000)
   private String content;
   // 작성 시간
   private LocalDateTime createdTime;
