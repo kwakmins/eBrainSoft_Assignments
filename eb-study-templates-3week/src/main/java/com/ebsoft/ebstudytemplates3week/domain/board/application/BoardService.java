f8ed73358952212a1d84a745ba052bbd0d03377d
kawksung <alstjd5479@naver.com>
2023-09-27 12:44:54 +0900

    chore: `MyBatis`에서 `@Transactional`사용 가능하게 설정

--- eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/application/BoardService.java
+++ eb-study-templates-3week/src/main/java/com/ebsoft/ebstudytemplates3week/domain/board/application/BoardService.java
@@ -4,9 +4,11 @@ import com.ebsoft.ebstudytemplates3week.domain.board.dao.BoardRepository;
 import com.ebsoft.ebstudytemplates3week.domain.board.dto.request.BoardWriteDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardService {
 
   private final BoardRepository boardRepository;
@@ -14,6 +16,7 @@ public class BoardService {
   /*
   게시판을 글쓰기 폼에서 받은 dto를 통해, 생성한다.
    */
+  @Transactional
   public void addBoard(BoardWriteDto reqDto) {
     boardRepository.writeBoard(reqDto);
   }
