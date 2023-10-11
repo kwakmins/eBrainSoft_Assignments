16fa9502d204b2884a15c804f5cf99ea42647292
kawksung <alstjd5479@naver.com>
2023-10-11 17:38:58 +0900

    (back)feat: Comment 관련 Entity 및 DAO 생성

---
+++ eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/domain/comment/entity/Comment.java
@@ -0,0 +1,41 @@
+package com.ebsoft.ebstudytemplates4weekbackend.domain.comment.entity;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import com.ebsoft.ebstudytemplates4weekbackend.domain.board.entity.Board;
+import com.ebsoft.ebstudytemplates4weekbackend.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "t_comment")
+public class Comment extends BaseEntity {
+
+  public static final int MAX_CONTENT_LENGTH = 255;
+
+  //댓글 id
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "comment_id", nullable = false)
+  private Long id;
+
+  //게시판 (N:1)
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "board_id", nullable = false)
+  private Board board;
+
+  //댓글 내용
+  @Column(name = "content", nullable = false, length = MAX_CONTENT_LENGTH)
+  private String content;
+}
