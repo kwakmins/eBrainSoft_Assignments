ac4df76dd955b81eb76adc57858717a61fc56379
kawksung <alstjd5479@naver.com>
2023-10-11 17:38:40 +0900

    (back)feat: Board 관련 Entity 및 DAO 생성

---
+++ eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/domain/board/dao/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.ebsoft.ebstudytemplates4weekbackend.domain.board.dao;
+
+import com.ebsoft.ebstudytemplates4weekbackend.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
