c968e0c99c6aed0d39e010355b4e97cd3369045b
kawksung <alstjd5479@naver.com>
2023-10-11 16:20:50 +0900

    (back)chore: 4주차 프로젝트 생성

---
+++ eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/EbStudyTemplates4weekBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ebsoft.ebstudytemplates4weekbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class EbStudyTemplates4weekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbStudyTemplates4weekBackendApplication.class, args);
+	}
+
+}
---
+++ eb-study-templates-4week-backend/src/test/java/com/ebsoft/ebstudytemplates4weekbackend/EbStudyTemplates4weekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebsoft.ebstudytemplates4weekbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbStudyTemplates4weekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
