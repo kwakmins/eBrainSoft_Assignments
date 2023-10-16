4a1a0616ae97b0dce7eb5948ca66c5424ef76664
kawksung <alstjd5479@naver.com>
2023-10-16 22:57:07 +0900

    refactor: `validation error message` 변경

--- eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/domain/board/dto/request/BoardWriteReqDto.java
+++ eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/domain/board/dto/request/BoardWriteReqDto.java
@@ -27,32 +27,36 @@ public class BoardWriteReqDto {
   public static final int MAX_CONTENT_LENGTH = 2000;
 
   // 카테고리
-  @NotNull
-  @Positive
+  @NotNull(message = "카테고리 아이디를 입력해주세요.")
+  @Positive(message = "올바른 카테고리 아이디를 입력해주세요.")
   private Long categoryId;
 
-  // 글쓴이
-  @NotBlank
-  @Length(min = MIN_USER_NAME_LENGTH, max = MAX_USER_NAME_LENGTH)
+  // 작성자 이름
+  @NotBlank(message = "작성자 이름을 입력해주세요.")
+  @Length(min = MIN_USER_NAME_LENGTH, max = MAX_USER_NAME_LENGTH
+      , message = "작성자 이름은 {min}자 이상 {max}자 이하로 입력해주세요.")
   private String userName;
 
   // 비밀번호
-  @NotBlank
-  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{4,16}$")
+  @NotBlank(message = "비밀번호를 입력해주세요.")
+  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{4,16}$"
+      , message = "비밀번호는 특수문자를 포함한 4~16자리 수 여야만 합니다.")
   private String password;
 
   // 비밀번호 확인
-  @NotBlank
+  @NotBlank(message = "비밀번호 확인을 입력해주세요.")
   private String passwordConfirm;
 
   // 제목
-  @NotBlank
-  @Length(min = MIN_TITLE_LENGTH, max = MAX_TITLE_LENGTH)
+  @NotBlank(message = "제목을 입력해주세요.")
+  @Length(min = MIN_TITLE_LENGTH, max = MAX_TITLE_LENGTH
+      , message = "제목은 {min}자 이상 {max}자 이하로 입력해주세요.")
   private String title;
 
   // 내용
-  @NotBlank
-  @Length(min = MIN_CONTENT_LENGTH, max = MAX_CONTENT_LENGTH)
+  @NotBlank(message = "내용을 입력해주세요.")
+  @Length(min = MIN_CONTENT_LENGTH, max = MAX_CONTENT_LENGTH
+      , message = "내용은 {min}자 이상 {max}자 이하로 입력해주세요.")
   private String content;
 
   /**
