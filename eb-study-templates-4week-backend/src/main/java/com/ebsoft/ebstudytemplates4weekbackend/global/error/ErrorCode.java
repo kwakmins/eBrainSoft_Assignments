84a979e69f28b5ce683f5d7677a2939559108e7b
kawksung <alstjd5479@naver.com>
2023-10-16 16:03:39 +0900

    feat: 에러 처리 response를 위한 enum 및 response Dto 생성

---
+++ eb-study-templates-4week-backend/src/main/java/com/ebsoft/ebstudytemplates4weekbackend/global/error/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.ebsoft.ebstudytemplates4weekbackend.global.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 오류 메시지와 상태를 쉽게 추가하기 위한 Enum
+ */
+@Getter
+public enum ErrorCode {
+  ;
+  //오류 메시지
+  private final String message;
+  //오류 상태코드
+  private final HttpStatus httpStatus;
+
+  ErrorCode(String message, HttpStatus httpStatus) {
+    this.message = message;
+    this.httpStatus = httpStatus;
+  }
+}
